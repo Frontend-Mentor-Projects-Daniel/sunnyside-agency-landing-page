0a1a2b087b17ba25aa0a5e32761be6ffe24d0ad4
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-09-27 13:24:56 -0400

    refactor navbar and hamburger menu

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,33 +1,14 @@
 var hamburgerMenu = document.getElementById('menu-toggle');
 var navBar = document.getElementById('nav-bar');
+var menuIsOpen = 'false';
 hamburgerMenu === null || hamburgerMenu === void 0 ? void 0 : hamburgerMenu.addEventListener('click', function (e) {
     var button = e.currentTarget;
     if (button != null) {
-        button.setAttribute('aria-expanded', 'true');
-    }
-    if (navBar != null && navBar.dataset.state !== 'mobile') {
-        navBar.dataset.state = 'mobile';
-        navBar.style.display = 'flex';
-    }
-    else if (navBar != null && navBar.dataset.state === 'mobile') {
-        button.setAttribute('aria-expanded', 'false');
-        navBar.dataset.state = 'hidden';
-        navBar.style.display = 'none';
+        button.setAttribute('aria-expanded', toggle(menuIsOpen));
     }
 });
-var body = document.body;
-var observer = new ResizeObserver(function (entries) {
-    var bodyElement = entries[0];
-    var isTablet = bodyElement.contentRect.width >= 768;
-    console.log(bodyElement.target);
-    if (navBar != null) {
-        navBar.dataset.state = isTablet ? 'desktop' : 'hidden';
-        if (navBar.dataset.state === 'hidden') {
-            navBar.style.display = 'none';
-        }
-        else {
-            navBar.style.display = 'flex';
-        }
-    }
-});
-observer.observe(body);
+function toggle(bool) {
+    var isTrue = !(bool === 'true');
+    menuIsOpen = isTrue.toString();
+    return menuIsOpen;
+}
--- develop/js/script.ts
+++ develop/js/script.ts
@@ -1,40 +1,18 @@
 const hamburgerMenu = document.getElementById('menu-toggle');
 const navBar = document.getElementById('nav-bar');
+let menuIsOpen = 'false';
 
 hamburgerMenu?.addEventListener('click', function (e) {
   const button = e.currentTarget as HTMLButtonElement;
 
   if (button != null) {
-    button.setAttribute('aria-expanded', 'true');
-  }
-
-  if (navBar != null && navBar.dataset.state !== 'mobile') {
-    navBar.dataset.state = 'mobile';
-    navBar.style.display = 'flex';
-  } else if (navBar != null && navBar.dataset.state === 'mobile') {
-    button.setAttribute('aria-expanded', 'false');
-
-    navBar.dataset.state = 'hidden';
-    navBar.style.display = 'none';
+    button.setAttribute('aria-expanded', toggle(menuIsOpen));
   }
 });
 
-const body = document.body;
+function toggle(bool: string): string {
+  const isTrue = !(bool === 'true');
+  menuIsOpen = isTrue.toString();
 
-const observer = new ResizeObserver((entries) => {
-  const bodyElement = entries[0];
-  const isTablet = bodyElement.contentRect.width >= 768;
-  console.log(bodyElement.target);
-
-  if (navBar != null) {
-    navBar.dataset.state = isTablet ? 'desktop' : 'hidden';
-
-    if (navBar.dataset.state === 'hidden') {
-      navBar.style.display = 'none';
-    } else {
-      navBar.style.display = 'flex';
-    }
-  }
-});
-
-observer.observe(body);
+  return menuIsOpen;
+}
